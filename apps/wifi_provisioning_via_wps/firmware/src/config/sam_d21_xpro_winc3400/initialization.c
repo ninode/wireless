b7f3ac80cdb3908b7545b8ad190fd42b43f30e7f
Ben Poon <Ben.Poon@microchip.com>
2019-12-05 16:37:16 +0800

    [WSGA-11668]: fix the sys_initialize() hang up issue

--- apps/wifi_provisioning_via_wps/firmware/src/config/sam_d21_xpro_winc3400/initialization.c
+++ apps/wifi_provisioning_via_wps/firmware/src/config/sam_d21_xpro_winc3400/initialization.c
@@ -272,7 +272,8 @@ const SYS_CONSOLE_INIT sysConsole0Init =
 
 void SYS_Initialize ( void* data )
 {
-  
+    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3);
+
     PORT_Initialize();
 
     CLOCK_Initialize();
