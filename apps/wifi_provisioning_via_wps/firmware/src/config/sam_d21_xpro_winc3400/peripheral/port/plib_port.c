a765705c4d3997a4670a67baa1ee79979e62b4e6
Hariram Ramesh <hariram.ramesh@microchip.com>
2020-09-03 18:11:28 +0530

    WSGA-12630: Added NVMCTRL plib

--- apps/wifi_provisioning_via_wps/firmware/src/config/sam_d21_xpro_winc3400/peripheral/port/plib_port.c
+++ apps/wifi_provisioning_via_wps/firmware/src/config/sam_d21_xpro_winc3400/peripheral/port/plib_port.c
@@ -94,8 +94,9 @@ void PORT_Initialize(void)
                 
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0xa2;
-   PORT_REGS->GROUP[1].PORT_OUT = 0xa2;
+   PORT_REGS->GROUP[1].PORT_OUT = 0x22;
    PORT_REGS->GROUP[1].PORT_PINCFG[4] = 0x1;
+   PORT_REGS->GROUP[1].PORT_PINCFG[7] = 0x2;
 
                 
 }
