fcbe245966bf4ff3adbd668618dc76464dc30828
Hariram Ramesh <hariram.ramesh@microchip.com>
2020-09-22 15:45:33 +0530

    Revert "WHOST-241: Fixed timing related issues by adding delay in example.c as a workaround for "ap_scan" and "wifi_tcp_server_in_softap" applications for SAME54 + WINC3400."
    This reverts commit 8279bfe972ae47afd165680013ddeca48e768c50.

--- apps/ap_scan/firmware/src/example.c
+++ apps/ap_scan/firmware/src/example.c
@@ -157,11 +157,6 @@ void APP_ExampleInitialize(DRV_HANDLE handle)
     APP_DebugPrintf("WINC AP Scan Example\r\n");
     APP_DebugPrintf("=========================\r\n");
 
-    /* This delay is a temporary workaround and will be replaced with required clock settings,
-        taking into account the SLEEP TIMER CALIBRATION cycle for different MCUs. */
-
-    WDRV_MSDelay(100);
-
     state = EXAMP_STATE_INIT;
 }
 
